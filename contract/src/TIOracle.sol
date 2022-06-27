2cefbb62310ee07a46aff8ddc6be75c7d1571d50
andysun <andy@tokeninsight.cn>
2022-06-27 15:01:15 +0800

    The basic framework of smart contracts that support rotating price feeds

---
+++ contract/src/TIOracle.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// TIOracle is an oracle that provides reliable prices in multiple currencies
+contract TIOracle {
+    // PriceInfo is a single piece of price information,
+    // which includes TI's quotation, on-chain DEX quotation, and the timestamp of price feeding
+    struct PriceInfo {
+        uint256 tiPrice;
+        uint256 dexPrice;
+        uint256 timestamp;
+    }
+    // coin name => price, with 8 digits of precision
+    mapping(string => PriceInfo) public lastPrice;
+    // last round
+    uint256 public lastRound;
+    // count
+    uint256 public feedCount;
+    // owner of the contract
+    address admin;
+    // list of transmission nodes
+    address[] public nodes;
+    // count per round
+    uint256 public countPerRound;
+    constructor() {
+        admin = msg.sender;
+        countPerRound = 5;
+    }
+
+    // get the current price in Uniswap, and the quote should be usdc
+    function estimateDexPrice(string memory coinName) public view returns (uint256) {
+        //TODO call uniswap to estimate
+        return 123;
+    }
+
+    // compare two prices from TI and Uniswap
+    function crossValidate(uint256 tiPrice, uint256 dexPrice) internal pure returns (bool) {
+        // TODO do cross validation
+        return tiPrice > 0 && dexPrice > 0;
+    }
+
+    //  decide next valid node to feed price, in a round-robbin way
+    function decideValidNode(uint256 roundNo) public view returns (address) {
+        require(nodes.length > 0, "list of transmission nodes is empty");
+        uint256 offset = roundNo % nodes.length;
+        return nodes[offset];
+    }
+
+    function isMyTurn() public view returns (bool)  {
+        return decideValidNode(lastRound) == msg.sender;
+    }
+
+    // feedPrice is called by transmission nodes to feed price of cryptos
+    function feedPrice(string memory coinName, uint256 price) public {
+        address validNode = decideValidNode(lastRound);
+        require(msg.sender == validNode, "invalid transmission node");
+        ++feedCount;
+        if (feedCount % countPerRound == 0) {
+            ++lastRound;
+        }
+        PriceInfo memory priceInfo;
+        priceInfo.tiPrice = price;
+        priceInfo.timestamp = block.timestamp;
+        uint256 dexPrice = estimateDexPrice(coinName);
+        require(crossValidate(price, dexPrice), "This price deviates too much from Uniswap and is rejected for submission");
+        priceInfo.dexPrice = dexPrice;
+        lastPrice[coinName] = priceInfo;
+    }
+
+    // addNode add new trasmission node
+    function addNode(address newNode) public {
+        require(msg.sender == admin, "invalid caller to add new node");
+        nodes.push(newNode);
+    }
+
+    // removeNode remove trasmission node from whitelist
+    function removeNode(address rmNode) public {
+        require(msg.sender == admin, "invalid caller to remove node");
+        for(uint i=0; i<nodes.length; i++) {
+            if (nodes[i] == rmNode) {
+                nodes[i] = nodes[nodes.length-1];
+                nodes.pop();
+                break;
+            }
+        }
+    }
+
+    // transferOwnership transfer the ownership of this contract
+    function transferOwnership(address newOwner) public {
+        require(msg.sender == admin, "invalid caller to transfer ownership");
+        admin =  newOwner;
+    }
+}
---
+++ contract/test/TIOracle.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/TIOracle.sol";
+
+contract ContractTest is Test {
+    TIOracle tiOracle;
+    address nodeA;
+    address nodeB;
+    address nodeC;
+    function setUp() public {
+        tiOracle = new TIOracle();
+        nodeA = address(0x5A);
+        nodeB = address(0x5B);
+        nodeC = address(0x5C);
+    }
+
+    function testRoundOwner() public {
+        tiOracle.addNode(nodeA);
+        tiOracle.addNode(nodeB);
+        tiOracle.addNode(nodeC);
+        for(uint i=0;i<15;i++) {
+            address node = tiOracle.decideValidNode(i);
+            emit log_named_address("node",node);
+            if(i%3 == 0) {
+                assertEq(node, nodeA);
+            }
+            if(i%3 == 1) {
+                assertEq(node, nodeB);
+            }
+            if(i%3 == 2) {
+                assertEq(node, nodeC);
+            }
+        }
+    }
+
+    function testRemoveNode() public {
+        testRoundOwner();
+        tiOracle.removeNode(nodeB);
+        for(uint i=0;i<12;i++) {
+            address node = tiOracle.decideValidNode(i);
+            emit log_named_address("node",node);
+            if(i%2 == 0) {
+                assertEq(node, nodeA);
+            }
+            if(i%2 == 1) {
+                assertEq(node, nodeC);
+            }
+        }
+    }
+
+    function testFeedPrice() public {
+        Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        testRoundOwner();
+        vm.startPrank(nodeA);
+        tiOracle.feedPrice("btc", 123);
+        tiOracle.feedPrice("eth", 12345);
+        tiOracle.feedPrice("eth", 12345);
+        tiOracle.feedPrice("eth", 12345);
+        tiOracle.feedPrice("eth", 12345);
+        assertTrue(!tiOracle.isMyTurn());
+        vm.stopPrank();
+        //node a can only feed 5 times, and then it is nodeB's turn now
+        vm.startPrank(nodeB);
+        assertTrue(tiOracle.isMyTurn());
+        tiOracle.feedPrice("eth", 12345);
+        vm.stopPrank();
+    }
+}

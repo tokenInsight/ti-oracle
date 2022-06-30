21d832227f9ec39704fcd7d414f318d7b5466dbf
andysun <andy@tokeninsight.cn>
2022-06-30 22:08:09 +0800

    add unit test for sign & verify

--- contract/src/TIOracle.sol
+++ contract/src/TIOracle.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-// import "forge-std/Test.sol";
+//import "forge-std/Test.sol";
 // TIOracle is an oracle that provides reliable prices in multiple currencies
 contract TIOracle {
     // PriceInfo is a single piece of price information,
-    // which includes TI's quotation, on-chain DEX quotation, and the timestamp of price feeding
+    // which includes TI's quotation, and the timestamp of price feeding
     struct PriceInfo {
-        uint256 tiPrice;
+        uint256 tiPrice; //median of peers' price
         uint256 timestamp;
-        uint256 precision;
+    }
+    // PeerPriceFeed represents price reported by each peer, with nodes' signature
+    struct PeerPriceFeed {
+        address peerAddress;
+        bytes sig;
+        uint256 price;
+        uint256 timestamp;
     }
     event NodeAdded(address newNode);
     event NodeRemoved(address removedNode);
@@ -26,6 +32,8 @@ contract TIOracle {
     address admin;
     // list of transmission nodes
     address[] public nodes;
+    // map of nodes
+    mapping(address => uint) nodesOffset;
     // count per round
     uint256 public countPerRound;
     // proposals of kicking nodes
@@ -65,13 +73,30 @@ contract TIOracle {
         return decideValidNode(lastRound) == msg.sender;
     }
 
-    // feedPrice is called by transmission nodes to feed price of cryptos
-    function feedPrice(string memory coinName, uint256 price, uint256 precision) public {
+    // check whether the feeding has enough signatures from > 2/3 nodes
+    function checkSignatures(string memory coinName, PeerPriceFeed[] memory peersPrice) view internal returns (bool) {
+        uint signCount = 0;
+        uint256 prevPeerPrice = 0;
+        for (uint i=0; i<peersPrice.length; i++) {
+            PeerPriceFeed memory peer = peersPrice[i];
+            require(nodesOffset[peer.peerAddress] > 0, "peer not in valid list");
+            require(peer.price >= prevPeerPrice , "price list not soreted in increasing order");
+            bytes32 digest = keccak256(abi.encodePacked(coinName, peer.price, peer.timestamp));
+            address recovered = recoverSign(digest, peer.sig);
+            require(recovered == peer.peerAddress, "invalid signature");
+            ++signCount;
+            prevPeerPrice = peer.price;
+        }
+        return nodes.length * 2 / 3 < signCount ;
+    }
+
+    // feedPrice is called by leader node to feed price of cryptos, with a price list reported by all peers
+    function feedPrice(string memory coinName, PeerPriceFeed[] memory peersPrice) public {
         require(isMyTurn(), "invalid transmission node");
+        require(checkSignatures(coinName, peersPrice), "no enough signatures of nodes");
         PriceInfo memory priceInfo;
-        priceInfo.tiPrice = price;
+        priceInfo.tiPrice = peersPrice[peersPrice.length/2].price; //median
         priceInfo.timestamp = block.timestamp;
-        priceInfo.precision = precision;
         lastPrice[coinName] = priceInfo;
         lastTimestamp = block.timestamp;
         emit PriceFeed(lastRound, priceInfo);
@@ -85,20 +110,26 @@ contract TIOracle {
     function addNode(address newNode) public {
         require(msg.sender == admin, "invalid caller to add new node");
         nodes.push(newNode);
+        nodesOffset[newNode] = nodes.length;
         emit NodeAdded(newNode);
+    }
+
+    // execute removing of a node
+    function swapAndPop(address rmNode) internal {
+        uint offset = nodesOffset[rmNode];
+        require(offset > 0, "node not exsit");
+        address tailNode = nodes[nodes.length - 1];
+        nodes[offset-1] = tailNode;
+        nodesOffset[tailNode] = offset;
+        nodes.pop();
+        delete nodesOffset[rmNode];
     }
 
     // removeNode remove trasmission node from whitelist
     function removeNode(address rmNode) public {
         require(msg.sender == admin, "invalid caller to remove node");
-        for(uint i=0; i<nodes.length; i++) {
-            if (nodes[i] == rmNode) {
-                nodes[i] = nodes[nodes.length-1];
-                nodes.pop();
-                emit NodeRemoved(rmNode);
-                break;
-            }
-        }
+        swapAndPop(rmNode);
+        emit NodeRemoved(rmNode);
     }
 
     // kickNode remove trasmission node from whitelist
@@ -119,15 +150,8 @@ contract TIOracle {
         kickProposals[rmNode].push(msg.sender);
         // >2/3 agree
         if (nodes.length * 2 / 3 < kickProposals[rmNode].length) {
-            for(uint i=0; i<nodes.length; i++) {
-                if (nodes[i] == rmNode) {
-                    nodes[i] = nodes[nodes.length-1];
-                    nodes.pop();
-                    delete kickProposals[rmNode];
-                    emit NodeKicked(rmNode);
-                    break;
-                }
-            }
+            swapAndPop(rmNode);
+            emit NodeKicked(rmNode);
         }
     }
 
@@ -135,5 +159,36 @@ contract TIOracle {
     function transferOwnership(address newOwner) public {
         require(msg.sender == admin, "invalid caller to transfer ownership");
         admin =  newOwner;
+    }
+
+    //recover address from sign
+    function recoverSign(bytes32 hash, bytes memory sig) public pure returns (address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        //Check the signature length
+        if (sig.length != 65) {
+        return (address(0));
+        }
+
+        // Divide the signature in r, s and v variables
+        assembly {
+        r := mload(add(sig, 32))
+        s := mload(add(sig, 64))
+        v := byte(0, mload(add(sig, 96)))
+        }
+
+        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        if (v < 27) {
+        v += 27;
+        }
+
+        // If the version is correct return the signer address
+        if (v != 27 && v != 28) {
+        return (address(0));
+        } else {
+        return ecrecover(hash, v, r, s);
+        }
     }
 }
--- contract/test/TIOracle.t.sol
+++ contract/test/TIOracle.t.sol
@@ -11,9 +11,9 @@ contract ContractTest is Test {
     address nodeC;
     function setUp() public {
         tiOracle = new TIOracle(5, 300); //5 feed each round, max delay 300 seconds
-        nodeA = address(0x5A);
-        nodeB = address(0x5B);
-        nodeC = address(0x5C);
+        nodeA = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        nodeB = address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
+        nodeC = address(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC);
     }
 
     function testRoundOwner() public {
@@ -50,15 +50,63 @@ contract ContractTest is Test {
         }
     }
 
+    function fakeEthFeeds() internal view returns (TIOracle.PeerPriceFeed[] memory) {
+            TIOracle.PeerPriceFeed[] memory feeds = new TIOracle.PeerPriceFeed[](3);
+            TIOracle.PeerPriceFeed memory item;
+            item.peerAddress = nodeA;
+            item.price = 23456;
+            item.timestamp = 1656587035;
+            item.sig = hex'764b3b307faabd37ec72270b31b71af012a0f21528e5a581b7b9052a7edc023c69104ef5104ab0dec78f9ddd2a22429cf3e9a44255e3ffd78e70284bed75a8731b';
+            TIOracle.PeerPriceFeed memory item2;
+            item2.peerAddress = nodeB;
+            item2.price = 23457;
+            item2.timestamp = 1656587035;
+            item2.sig = hex'707b11750fabff7e2fb731e82fa69aa373118f2c41c711892e3284676a3b1af427d7a9423b8fbcb28963cee5d6a4e3c9ef87cb0b6fbe9a462dc096471228a0f41b';
+            TIOracle.PeerPriceFeed memory item3;
+            item3.peerAddress = nodeC;
+            item3.price = 23458;
+            item3.timestamp = 1656587035;
+            item3.sig = hex'64ae025ad2a1f1d7207407b7df9c61bb859b58daf9f3b397f791ee02d12602a12e933fbf4db53c317225df2f430927d6bc0ffa7d3d8525a070c4112ae87673681c';
+            feeds[0] = item;
+            feeds[1] = item2;
+            feeds[2] = item3;
+            return feeds;
+    }
+
+    function fakeBTCFeeds() internal view returns (TIOracle.PeerPriceFeed[] memory) {
+            TIOracle.PeerPriceFeed[] memory feeds = new TIOracle.PeerPriceFeed[](3);
+            TIOracle.PeerPriceFeed memory item;
+            item.peerAddress = nodeA;
+            item.price = 23456;
+            item.timestamp = 1656587035;
+            item.sig = hex'85991170e857be37efcfdbe57aacc27fe901465e1b1f71b3fe6fb2290e7ff001121c0ca78ded21dabd6cff104e3fe0bebed75ba9be8417144067eee3f64bb1a91b';
+            TIOracle.PeerPriceFeed memory item2;
+            item2.peerAddress = nodeB;
+            item2.price = 23457;
+            item2.timestamp = 1656587035;
+            item2.sig = hex'7271e8f1fe901e4f6707656e9be96e3acb958f4c3bd9c103719afa19ebf53862431c6d3f459ae91060a7e9b3d78c7ca2ef9d3dd03b95b1f1635012741c7617131c';
+            TIOracle.PeerPriceFeed memory item3;
+            item3.peerAddress = nodeC;
+            item3.price = 23458;
+            item3.timestamp = 1656587035;
+            item3.sig = hex'625801a969ef917ca023116b57d86aac67a82df84b1a8e1535bcf223244c4e475e0c6d4d7247713cd6e9ffb4c463d300d29223895d9fa17072dce05fdce829061c';
+            feeds[0] = item;
+            feeds[1] = item2;
+            feeds[2] = item3;
+            return feeds;
+    }
+
     function testFeedPrice() public {
         Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
         testRoundOwner();
         vm.startPrank(nodeA);
-        tiOracle.feedPrice("btc", 123, 1);
-        tiOracle.feedPrice("eth", 12345, 2);
-        tiOracle.feedPrice("eth", 12345, 2);
-        tiOracle.feedPrice("eth", 12345, 2);
-        tiOracle.feedPrice("eth", 12345, 2);
+        TIOracle.PeerPriceFeed[] memory btcFeeds = fakeBTCFeeds();
+        TIOracle.PeerPriceFeed[] memory ethFeeds = fakeEthFeeds();
+        tiOracle.feedPrice("btc", btcFeeds);
+        tiOracle.feedPrice("eth", ethFeeds);
+        tiOracle.feedPrice("eth", ethFeeds);
+        tiOracle.feedPrice("eth", ethFeeds);
+        tiOracle.feedPrice("eth", ethFeeds);
         assertTrue(!tiOracle.isMyTurn());
         //nodeA can only feed 5 times, and then it is nodeB's turn now
         //Here, we cheat to make it timeout, so that any nodes could feed
@@ -68,10 +116,10 @@ contract ContractTest is Test {
 
         vm.startPrank(nodeB);
         assertTrue(tiOracle.isMyTurn());
-        tiOracle.feedPrice("eth", 1234567, 2);
+        tiOracle.feedPrice("eth", ethFeeds);
         vm.stopPrank();
         TIOracle.PriceInfo memory ethPrice = tiOracle.queryPrice("eth");
-        assertEq(ethPrice.tiPrice, 1234567);
+        assertEq(ethPrice.tiPrice, 23457); //median
     }
 
     function testKickNode() public {
@@ -85,4 +133,10 @@ contract ContractTest is Test {
         tiOracle.kickNode(nodeC);
     }
 
+    function testVerify() public {
+        bytes memory  digest = hex'6865656c6c20776f726c640000000000000000000000000000000000000000000000000000000000003039';
+        bytes memory sign = hex'3571db3a6e9027358a0acd06a3596a4ba6307adde571ca97fb2416b208ec1ac015e2d8538a2ff270f10b253034ab1caffcd0e367ca7c3991c79ab4acc7af14321c';
+        address rec = tiOracle.recoverSign(keccak256(digest), sign);
+        emit log_named_address("recover", rec) ;
+    }
 }

13023ac89aec2b35cb8f8839f355a1e13eac99dc
fxsjy <ccnusjy@gmail.com>
2022-07-21 09:21:03 +0800

    adjust order of events displayed

--- node/src/processor/web.rs
+++ node/src/processor/web.rs
@@ -86,7 +86,8 @@ async fn peers(Extension(state): Extension<SharedState>) -> impl IntoResponse {
 }
 
 async fn events(Extension(state): Extension<SharedState>) -> impl IntoResponse {
-    let events = state.lock().unwrap().chain_events.clone();
+    let mut events = state.lock().unwrap().chain_events.clone();
+    events.reverse(); //show latest events first
     (StatusCode::ACCEPTED, Json(events))
 }
 
